f7c2b45f08d49171bc8d6b963700458e94915be2
iftikhar <iftiiihasan@gmail.com>
2024-09-01 23:19:41 +0600

    clock app

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
-// git --version
 
-// git config --global user.name "iftakhar hasan"
-// git config --global user.email "iftakharhasan2021@gmail.com"
-// git config --list
+const clock = document.querySelector('#clock');
 
-// first step --staging area:
-//    git add filename
-//    git add .
+function updateTime() {
 
-// second step --commit(saved)
-// git commit -m 'message'
-// git commit -am **
+    const now = new Date();
+    const hours = now.getHours() > 12 ? now.getHours() - 12 : now.getHours()
+    const minutes = now.getMinutes()
+    const seconds = now.getSeconds()
+    const time = `${ hours.toString().padStart(2,'0') }: ${ minutes.toString().padStart(2,'0') }: ${ seconds.toString().padStart(2,'0') } ${ hours > 12 ? "pm" : "am"}`;
 
+    clock.innerText = time; 
 
-// important command
-// git init
-// git status 
-// git log -oneline
-// git show commit_version
-// git reset
-// git reset --hard
-// git checkout version
-// git clone
-// git push
+}
+
+updateTime();
+setInterval(updateTime, 1000);
