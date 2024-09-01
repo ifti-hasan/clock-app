60409bfd2bfc4c9f59474a5fd2b38d2ee006e0e9
iftikhar <iftiiihasan@gmail.com>
2024-09-01 13:40:48 +0600

    Added all

---
+++ app.js
@@ -0,0 +1,25 @@
+// git --version
+
+// git config --global user.name "iftakhar hasan"
+// git config --global user.email "iftakharhasan2021@gmail.com"
+// git config --list
+
+// first step --staging area:
+//    git add filename
+//    git add .
+
+// second step --commit(saved)
+// git commit -m 'message'
+// git commit -am **
+
+
+// important command
+// git init
+// git status 
+// git log -oneline
+// git show commit_version
+// git reset
+// git reset --hard
+// git checkout version
+// git clone
+// git push
